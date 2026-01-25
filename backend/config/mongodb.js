581a0f879dd8fbc5a1be4308338fee43770765d7
Sandesh <25temporaryy@gmail.com>
2026-01-26 02:02:43 +0530

    provided cloud mongo uri

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -7,6 +7,7 @@ main()
     console.log(err)
 })
 async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/imagify')
+    const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/imagify';
+    await mongoose.connect(uri);
 }
 export default main;
